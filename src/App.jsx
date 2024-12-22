17b5061efa00b230f7f06d5a72e27e53f887ff9c
HarshlataGajbhiye <harshlatagajbhiye3@gmail.com>
2024-12-22 16:05:15 +0530

    show less and more is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import Card from "./components/Card";
-import "./components/index.css";
+import "./index.css";
+import TextExpander from "./components/TextExpander";
 const appStyle = {
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
-    height: "100vh",
+    flexDirection: "column",
+    gap: "rrem",
+    padding: "4rem 0",
 };
 function App() {
     return (
         <div style={appStyle}>
             <Card />
+            <TextExpander />
         </div>
     );
 }
---
+++ src/components/TextExpander.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+const textStyle = {
+    maxWidth: "40rem",
+};
+
+const showButton = {
+    backgroundColor: "#9C36B5",
+    color: "white",
+    border: "none",
+    borderRadius: "1rem",
+    padding: " 0.5rem  1rem",
+};
+const rowText = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+                sequi autem optio ipsum sed cum incidunt, harum repellat dicta
+                quaerat quam qui eius repellendus. Porro vitae magni excepturi
+                tempora ut doloribus illum, doloribus consequuntur fuga, labore,
+                molestias obcaecati eveniet impedit laboriosam neque vero. Nulla
+                doloremque quia consectetur voluptatem perferendis, minus natus
+                aliquid mollitia quis autem id, iusto aspernatur vero laboriosam
+                quas? Quaerat non sapiente quos fugiat? Error, cum voluptates
+                explicabo eos fugit deleniti blanditiis vero. Quam nisi quod
+                facilis facere officiis. Rem, accusamus, corrupti dicta
+                necessitatibus ipsam sint adipisci doloribus perspiciatis
+                delectus quam quod esse, voluptatem ipsum dolor sit amet
+                consectetur adipisicing elit. `;
+
+function TextExpander() {
+    const [show, setShow] = useState(false);
+    //derived state
+    const text = show ? rowText : rowText.slice(0, 100);
+
+    function handleClick() {
+        setShow((show) => !show);
+    }
+    return (
+        <div style={textStyle}>
+            <p>{text} </p>
+            <button onClick={handleClick} style={showButton}>
+                {show ? "showless" : "showmore"}
+            </button>
+        </div>
+    );
+}
+
+export default TextExpander;
