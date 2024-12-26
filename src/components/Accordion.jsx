2a61ea6585cf3d383d5f8b77e8d0245fd47d3abb
HarshlataGajbhiye <harshlatagajbhiye3@gmail.com>
2024-12-26 18:27:49 +0530

    Accordion project done

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,18 +1,36 @@
 import { useState } from "react";
 import styles from "../style/Accordion.module.css";
+import { HiMiniMinus, HiMiniPlus } from "react-icons/hi2";
 
 function Accordion(props) {
+    // App.jsx main hai  parent hai jiski propertity ko child me access kar rahe hai or props ke throw kar sakte hai
     const { content, color } = props;
 
-    const [open, setOpen] = useState(false);
+    const [open, setOpen] = useState(null);
+
+    //open == null > all closed
+    //open == 1 > 1sth row is opend
+    // isOpend= open ===index
+
+    // curr index
+    // 1    1  = null
+    // 1    2  = 2
+
+    function handleClick(index) {
+        setOpen((currState) => (currState === index ? null : index));
+    }
 
     return (
-        <div>
+        <div className={styles.container}>
             {content.map(function (content, index) {
                 return (
                     <Row
+                        // value is pass
                         title={content.title}
                         details={content.details}
+                        handleClick={handleClick}
+                        index={index}
+                        isOpen={open === index}
                         key={index}
                     />
                 );
@@ -21,11 +39,17 @@ function Accordion(props) {
     );
 }
 function Row(props) {
-    const { title, details } = props;
+    const { title, details, handleClick, index, isOpen } = props;
+    //receive kiya hai title and details
     return (
-        <div>
-            <h3>{title}</h3>
-            <p>{details}</p>
+        <div className={styles.row}>
+            <h3 onClick={() => handleClick(index)} className={styles.title}>
+                <span>
+                    {title}
+                    {isOpen ? <HiMiniMinus /> : <HiMiniPlus />}
+                </span>
+            </h3>
+            {isOpen && <p className={styles.details}>{details}</p>}
         </div>
     );
 }
