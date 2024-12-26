d26475c425f9496569fb67dbd0a87bf108f806bb
HarshlataGajbhiye <harshlatagajbhiye3@gmail.com>
2024-12-26 16:34:59 +0530

    Started-Accordion

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,31 @@ import "./index.css";
 import TextExpander from "./components/TextExpander";
 import DateCounter from "./components/dateCounter";
 import StarRating from "./components/StarRating";
+import Accordion from "./components/Accordion";
+
+const accordionContent = [
+    {
+        title: "About Hiper-Text-Markup-Language",
+        details:
+            "Hypertext Markup Language is the standard markup language for documents designed to be displayed in a web browser. It defines the content and structure of web content.",
+    },
+    {
+        title: "About Casceding-Style-Sheet",
+        details:
+            "Cascading Style Sheets (CSS) is a stylesheet language used to describe the presentation of a document written in HTML or XML (including XML dialects such as SVG, MathML or XHTML). CSS describes how elements should be rendered on screen, on paper, in speech, or on other media.",
+    },
+    {
+        title: "About Java-Script",
+        details:
+            "JavaScript (JS) is a lightweight interpreted (or just-in-time compiled) programming language with first-class functions. While it is most well-known as the scripting language for Web pages, many non-browser environments also use it, such as Node.js, Apache CouchDB and Adobe Acrobat. JavaScript is a prototype-based, multi-paradigm, single-threaded, dynamic language, supporting object-oriented, imperative, and declarative (e.g. functional programming) styles.",
+    },
+    {
+        title: "About React",
+        details:
+            "React lets you build user interfaces out of individual pieces called components. Create your own React components like Thumbnail, LikeButton, and Video. Then combine them into entire screens, pages, and apps.",
+    },
+];
+
 const appStyle = {
     display: "flex",
     alignItems: "center",
@@ -22,6 +47,7 @@ function App() {
                 <StarRating color="#ffd43b" defaultRating={4} size={6} />
                 <StarRating color="#40c057" defaultRating={5} size={9} />
             </div>
+            <Accordion content={accordionContent} color="#2f9e44" />
         </div>
     );
 }
---
+++ src/components/Accordion.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import styles from "../style/Accordion.module.css";
+
+function Accordion(props) {
+    const { content, color } = props;
+
+    const [open, setOpen] = useState(false);
+
+    return (
+        <div>
+            {content.map(function (content, index) {
+                return (
+                    <Row
+                        title={content.title}
+                        details={content.details}
+                        key={index}
+                    />
+                );
+            })}
+        </div>
+    );
+}
+function Row(props) {
+    const { title, details } = props;
+    return (
+        <div>
+            <h3>{title}</h3>
+            <p>{details}</p>
+        </div>
+    );
+}
+
+export default Accordion;
